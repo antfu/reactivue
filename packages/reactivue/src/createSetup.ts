d545d7bea56b68c260a8b990bbd8e9dd6bfdebd6
Dave.rs <davidiazfz@gmail.com>
2020-11-21 20:10:58 -0500

    Making optional useSetup/createSetup props argument.

--- packages/reactivue/src/createSetup.ts
+++ packages/reactivue/src/createSetup.ts
@@ -1,3 +1,3 @@
 import { useSetup } from './useSetup'
 
-export const createSetup = <Props, State>(setupFn: (props: Props) => State) => (props: Props) => useSetup(setupFn, props)
+export const createSetup = <Props, State>(setupFn: (props?: Props) => State) => (props: Props) => useSetup(setupFn, props)
