6f39d580b442089a7cbd11cebe66deb269c67fa1
sibbng <sibbngheid@gmail.com>
2021-02-10 21:47:43 +0300

    fix: purge script and upgrade tailwind

--- examples/react-ts-vite/tailwind.config.js
+++ examples/react-ts-vite/tailwind.config.js
@@ -2,16 +2,10 @@ module.exports = {
   purge: {
     content: [
       './index.html',
-      './src/**/*.vue',
-      './src/**/*.js',
-      './src/**/*.ts',
+      './src/**/*.tsx',
     ],
   },
   theme: {
-    extend: {
-      opacity: {
-        10: '0.1',
-      },
-    },
+    extend: {},
   },
 }
