a38577ca2626e9db2b2975782135f2fe6ed81157
sibbng <sibbngheid@gmail.com>
2022-03-05 22:25:42 +0300

    feat: patch `VNode` type (https://github.com/vuejs/core/issues/1033)

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,13 +4,16 @@ import { join } from 'path'
 const require = createRequire(import.meta.url)
 const runtime = require.resolve('@vue/runtime-core')
 const runtimeDist = join(runtime, '../dist')
-const files = (await readdir(runtimeDist)).filter(file => file.endsWith('.js'))
+const files = await readdir(runtimeDist)
 const exports = ['setCurrentInstance', 'unsetCurrentInstance', 'currentInstance', 'createHook']
 const getExports = isEsm => exports.map(e => isEsm ? `export { ${e} };` : `exports.${e} = ${e};`).join('\n')
 for (const file of files) {
   let content = await readFile(join(runtimeDist, file), 'utf8')
-  if (content.match(/export.*setCurrentInstance/))
-    continue
-  content += `\n${getExports(file.includes('esm'))}`
+  if (file.endsWith('js') && !content.match(/export.*setCurrentInstance/))
+    content += `\n${getExports(file.includes('esm'))}`
+
+  if (file.endsWith('d.ts') && !content.match(/VNodeDisabled/))
+    content = content.replace(/export declare interface VNode/g, 'export declare interface VNodeDisabled')
+
   writeFile(join(runtimeDist, file), content, 'utf8')
 }
